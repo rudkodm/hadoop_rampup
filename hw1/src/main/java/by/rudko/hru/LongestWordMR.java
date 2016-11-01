6e3d79ce560378411e59246c4dd3284128157c5c
rudkodm <rudko.d.v@gmail.com>
2016-11-01 19:49:23 +0300

    Issue #1 - implemented 'main' method.

--- hw1/src/main/java/by/rudko/hru/LongestWordMR.java
+++ hw1/src/main/java/by/rudko/hru/LongestWordMR.java
@@ -1,9 +1,14 @@
 package by.rudko.hru;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.Mapper;
 import org.apache.hadoop.mapreduce.Reducer;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -75,8 +80,18 @@ public class LongestWordMR {
     }
 
     // Main
-    public static void main(String...args) {
-
+    public static void main(String...args) throws Exception {
+        Configuration conf = new Configuration();
+        Job job = Job.getInstance(conf, "Larges Word");
+        job.setJarByClass(LongestWordMR.class);
+        job.setMapperClass(LongestWordMapper.class);
+        job.setCombinerClass(LongestWordReducer.class);
+        job.setReducerClass(LongestWordReducer.class);
+        job.setOutputKeyClass(Text.class);
+        job.setOutputValueClass(IntWritable.class);
+        FileInputFormat.addInputPath(job, new Path(args[0]));
+        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        System.exit(job.waitForCompletion(true) ? 0 : 1);
     }
 }
 
